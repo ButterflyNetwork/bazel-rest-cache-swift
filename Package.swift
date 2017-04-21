57521ad769cc151898a82a64563044f0e7d2b3d0
Jason Gavris <jgavris@butterflynetinc.com>
2017-04-21 18:42:36 -0400

    Update Kitura

--- Package.swift
+++ Package.swift
@@ -6,17 +6,21 @@ let package = Package(
         .Package(
             url: "https://github.com/IBM-Swift/Kitura.git",
             majorVersion: 1,
-            minor: 4
+            minor: 7
         ),
         .Package(
             url: "https://github.com/IBM-Swift/Kitura-redis.git",
             majorVersion: 1,
-            minor: 4
+            minor: 7
         ),
         .Package(
             url: "https://github.com/IBM-Swift/HeliumLogger.git",
             majorVersion: 1,
-            minor: 4
+            minor: 7
+        ),
+        .Package(
+            url: "https://github.com/IBM-Swift/SwiftyJSON.git", 
+            majorVersion: 16
         ),
     ]
 )
