c3261350bd159e7673b59d3831e1ab3371e836a5
Jason Gavris <jgavris@butterflynetinc.com>
2017-05-16 23:56:48 -0400

    [Bazel] Add redis backed cache

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,12 +6,17 @@ import SwiftRedis
 
 HeliumLogger.use()
 
-let router = Router()
+let router: Router = {
+    let router = Router()
+    router.all(middleware: BodyParser())
+    return router
+}()
 
 func redis() -> Redis {
     let redis = Redis()
     let redisUrl = URL(string: ProcessInfo.processInfo.environment["REDIS_URL"]!)!
-    Log.info("Redis: Connecting to \(redisUrl)")
+
+    let sem = DispatchSemaphore(value: 0)
 
     redis.connect(
         host: redisUrl.host!,
@@ -21,41 +26,116 @@ func redis() -> Redis {
             Log.error("Redis: \(error)")
             fatalError()
         } else {
-            Log.info("Redis: Connected to \(redisUrl)")
-
-            redis.auth(redisUrl.password!) { (error) in
-                if let error = error {
-                    Log.error("Redis: Failed to authenticate \(error)")
-                    fatalError()
-                } else {
-                    Log.info("Redis: Authenticated")
+            if let password = redisUrl.password {
+                redis.auth(password) { (error) in
+                    if let error = error {
+                        Log.error("Redis: Failed to authenticate \(error)")
+                        fatalError()
+                    } else {
+                        _ = sem.signal()
+                    }
                 }
+            } else {
+                sem.signal()
             }
         }
     }
+
+    sem.wait()
 
     return redis
 }
 
-router.get("/") { request, response, next in
-    redis().incr("HELLO_WORLD") { (value, error) in
-        defer { next() }
+router.head("/cache/:key") { request, response, next in
+    Log.info("HEAD: \(request.parameters["key"] ?? "")")
 
-        if let error = error {
-            Log.error("Redis: \(error)")
-            response.send("Sad world :(")
-        } else {
-            if let value = value {
-                response.send("Hello, World! #\(value)")
+    if let key = request.parameters["key"] {
+        redis().exists(key) { (count, err) in
+            if let count = count, count > 0 {
+                Log.info("Hit: \(request.parameters["key"] ?? "")")
+                response
+                    .status(.OK)
+                    .send("")
             } else {
-                response.send("Sad world :(")
+                Log.info("Miss: \(request.parameters["key"] ?? "")")
+                response
+                    .status(.notFound)
+                    .send("")
             }
+
+            next()
         }
+    } else {
+        response
+            .status(.badRequest)
+            .send("")
+        next()
+    }
+}
+
+router.get("/cache/:key") { request, response, next in
+    Log.info("GET: \(request.parameters["key"] ?? "")")
+
+    if let key = request.parameters["key"] {
+        redis().get(key) { (str, err) in
+            if let data = str?.asData {
+                Log.info("Hit: \(request.parameters["key"] ?? "")")
+                response
+                    .status(.OK)
+                    .send(data: data)
+            } else {
+                Log.info("Miss: \(request.parameters["key"] ?? "")")
+                response
+                    .status(.notFound)
+                    .send("")
+            }
+
+            next()
+        }
+    } else {
+        response
+            .status(.badRequest)
+            .send("")
+        next()
+    }
+}
+
+router.put("/cache/:key") { request, response, next in
+    Log.info("PUT: \(request.parameters["key"] ?? "")")
+
+    if let key = request.parameters["key"],
+       let body = request.body {
+        switch body {
+        case .raw(let data):
+            redis().set(key, value: RedisString(data)) { (didSet, err) in
+                if didSet {
+                    response
+                        .status(.OK)
+                        .send("")
+                } else {
+                    response
+                        .status(.internalServerError)
+                        .send("")
+                }
+
+                next()
+            }
+        default:
+            response
+                .status(.badRequest)
+                .send("")
+            next()
+        }
+    } else {
+        response
+            .status(.badRequest)
+            .send("")
+        next()
     }
 }
 
 Kitura.addHTTPServer(
-    onPort: Int(ProcessInfo.processInfo.environment["PORT"] ?? "8090") ?? 8090,
+    onPort: Int(ProcessInfo.processInfo.environment["PORT"] ?? "8080") ?? 8080,
     with: router
 )
 
